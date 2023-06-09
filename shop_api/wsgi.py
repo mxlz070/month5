d5fd4fb04929e841a0dc6d4ba8354d380426ee43
mxlz070 <mxlz070@gmail.com>
2023-06-09 19:00:21 +0300

    hw 2-3-4

--- shop_api/wsgi.py
+++ shop_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dop_lesson_5 project.
+WSGI config for shop_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dop_lesson_5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_api.settings')
 
 application = get_wsgi_application()
